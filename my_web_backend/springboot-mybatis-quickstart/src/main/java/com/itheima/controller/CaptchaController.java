7f27397fa3d4b06fbdec965a0b753f773b39341b
RinzeSAMA <2053312368@qq.com>
2024-11-27 15:12:39 +0800

    修改了验证码的处理逻辑

--- my_web_backend/springboot-mybatis-quickstart/src/main/java/com/itheima/controller/CaptchaController.java
+++ my_web_backend/springboot-mybatis-quickstart/src/main/java/com/itheima/controller/CaptchaController.java
@@ -3,20 +3,27 @@ package com.itheima.controller;
 import com.itheima.pojo.Result;
 import com.itheima.pojo.VerifyCode;
 import com.itheima.pojo.VerifyCodeCheck;
-import com.itheima.utils.redis.RedisUtil;
+import com.itheima.utils.redis.CacheClient;
 import com.wf.captcha.SpecCaptcha;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.itheima.utils.redis.RedisConstants.LOGIN_CAPTCHA_KEY;
+import static com.itheima.utils.redis.RedisConstants.LOGIN_CAPTCHA_TTL;
 
 @RestController
 public class CaptchaController {
     @Autowired
-    private RedisUtil redisUtil;
-
+    private CacheClient cacheClient;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("/captcha/get")
     public Result captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -27,7 +34,7 @@ public class CaptchaController {
         String verCode = specCaptcha.text().toLowerCase();
         String key = UUID.randomUUID().toString();
         // 存入redis并设置过期时间为2分钟
-        redisUtil.set(key, verCode, 180);
+       cacheClient.setWithAutoExpire(LOGIN_CAPTCHA_KEY + key,verCode,LOGIN_CAPTCHA_TTL, TimeUnit.MINUTES);
         // 将key和base64返回给前端
         VerifyCode verifyCode = new VerifyCode(key,specCaptcha.toBase64());
         return Result.success(verifyCode);
@@ -38,7 +45,7 @@ public class CaptchaController {
     public Result login(@RequestBody VerifyCodeCheck verifyCodeCheck){
 
         // 根据Verkey获取redis中的对应的验证码
-        String redisCode = (String) redisUtil.get(verifyCodeCheck.getKey());
+        String redisCode = (String) stringRedisTemplate.opsForValue().get(LOGIN_CAPTCHA_KEY + verifyCodeCheck.getKey());
         System.out.println(verifyCodeCheck.getKey());
         System.out.println(verifyCodeCheck.getInputCode());
         System.out.println(redisCode);
