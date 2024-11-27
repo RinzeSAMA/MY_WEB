b6848240d2ab0dfb118a96db6e63121373107cc3
RinzeSAMA <2053312368@qq.com>
2024-11-27 15:37:26 +0800

    换为了新的Redis工具类

--- my_web_backend/springboot-mybatis-quickstart/src/main/java/com/itheima/controller/LoginController.java
+++ my_web_backend/springboot-mybatis-quickstart/src/main/java/com/itheima/controller/LoginController.java
@@ -5,7 +5,6 @@ import com.itheima.pojo.User;
 import com.itheima.pojo.VerifyCode;
 import com.itheima.service.UserService;
 import com.itheima.utils.JwtUtils;
-import com.itheima.utils.redis.RedisUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
